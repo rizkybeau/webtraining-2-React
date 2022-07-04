0c52bb2ba39fdda094e8974a7242fc7207fd70cd
rizkybeau <rizki.gushendrawan@gmail.com>
2022-07-04 16:15:57 +0700

    Add aboutUs components

--- src/constant/images.js
+++ src/constant/images.js
@@ -3,9 +3,13 @@ import gericht from '../assets/gericht.png';
 import spoon from '../assets/spoon.png';
 import logo from '../assets/logo.svg';
 import banner from '../assets/banner.jpg';
+import teams from '../assets/aboutUs.jpg';
+import history from '../assets/OurHistory.jpg';
 export default {
   spoon,
+  teams,
   logo,
+  history,
   gericht,
   banner,
 };
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,13 +1,38 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
+import { images } from '../../constant';
 import './AboutUs.css';
+// <h1 className="headtext__cormorant">Our History</h1>
+//       <img src={images.history} />
 const AboutUs = () => {
   return (
-    <div className="deskripsi-width">
-      <h1 className="head">Wujudkan Rumah impianmu bersama kami</h1>
-      <h1 className="head">
-        Membangun rumah dari nol bersama kami. kami memiliki pengalaman yang
-        sangat lama dan klien kami sudah berbagai macam daerah di indonesia.
-      </h1>
+    <div className="app__bg">
+      <div className="app__wrapper app__bg  section__padding">
+        <div className="app__wrapper_img app__wrapper_img-reverse">
+          <img src={images.teams} />
+        </div>
+        <div className="app__wrapper_info">
+          <h1 className="headtext__cormorant">About Us</h1>
+          <p className="p__opensans">
+            Membangun rumah dari nol bersama kami. kami memiliki pengalaman
+            dibidang konstruksi yang sangat lama dan klien kami sudah berbagai
+            macam daerah di indonesia.{' '}
+          </p>
+        </div>
+      </div>
+      <div className="app__wrapper section__padding">
+        <div className="app__wrapper_info">
+          <h1 className="headtext__cormorant">Our History</h1>
+          <p className="p__opensans">
+            Membangun rumah dari nol bersama kami. kami memiliki pengalaman
+            dibidang konstruksi yang sangat lama dan klien kami sudah berbagai
+            macam daerah di indonesia.{' '}
+          </p>
+        </div>
+        <div className="app__wrapper_img">
+          <img src={images.history} />
+        </div>
+      </div>
     </div>
   );
 };
