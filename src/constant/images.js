8d3caa6f6f3f8866fa8d9917d2ace176e247d8f6
rizkybeau <rizki.gushendrawan@gmail.com>
2022-07-06 18:49:36 +0700

    add Gallery container

--- src/constant/images.js
+++ src/constant/images.js
@@ -5,8 +5,14 @@ import logo from '../assets/logo.svg';
 import banner from '../assets/banner.jpg';
 import teams from '../assets/aboutUs.jpg';
 import history from '../assets/OurHistory.jpg';
+import gallery01 from '../assets/rumah.jpg';
+import gallery02 from '../assets/tempat-tidur.jpg';
+import gallery04 from '../assets/rumah04.jpg';
 export default {
   spoon,
+  gallery01,
+  gallery02,
+  gallery04,
   teams,
   logo,
   history,
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import './Gallery.css';
-// import {image} from '../../constant'
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs';
+
+import { images } from '../../constant';
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
   return (
-    <div className="app__wrapper app__bg section__padding">
-      <h1 className="headtext__cormorant">Gallery</h1>
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <h1 className="headtext__cormorant">instagram</h1>
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {[
+            images.gallery01,
+            images.gallery02,
+            images.banner,
+            images.gallery04,
+          ].map((image, index) => (
+            <div
+              className="app__gallery-images_card flex__center"
+              key={`gallery_image-${index + 1}`}
+            >
+              <img src={image} alt="gallery_image" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </div>
     </div>
   );
 };
